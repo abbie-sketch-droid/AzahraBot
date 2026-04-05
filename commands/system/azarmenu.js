63d37fcf6bf12dddd218007076696ff82669a640
azartech <mohamedazarudeen011@gmail.com>
2026-04-05 19:52:11 +0530

    updated

---
+++ commands/system/azarmenu.js
@@ -0,0 +1,6 @@
+// ==============================================
+// ⚙️ Azahrabot Fast Router for Obfuscated Menu
+// Points to the encrypted source in utils/azarmenu.js
+// ==============================================
+
+module.exports = require("../../utils/azarmenu.js");
